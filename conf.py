fb853d05eb4216172af3cfb73af724c43ed4e4d5
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-08-04 14:35:41 +0300

    first commit

---
+++ conf.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import telepot
+
+TOKEN = '245698813:AAGAwMUKnAk6Zo5pcqSPOzPmSgdaqrmXJs8'
+
+bot = telepot.Bot(TOKEN)
+
+ABUSES = {'adjectives': ('моченый', 'копченый', 'гальванический', 'метафизический', 'веселый',
+                         'печальный', 'задорный', 'загадочный', 'пригожий', 'задумчивый',),
+          'nouns': ('хуй', 'дрищ', 'лох', 'ботан')}
---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from http import client
+import json
+from random import choice
+
+from bottle import run, post, HTTPError, request, response
+
+from conf import bot, ABUSES, TOKEN
+
+
+@post('/<bot_token>')
+def index(bot_token):
+    if bot_token != TOKEN:
+        return HTTPError(client.FORBIDDEN, 'Bad token')
+    try:
+        body = json.loads(request.body.read().decode())
+    except ValueError:
+        return HTTPError(client.BAD_REQUEST, 'Invalid request body')
+
+    chat_id = body['message']['chat']['id']
+
+    resp = 'ты {1} {0}'.format(choice(ABUSES['nouns']), choice(ABUSES['adjectives']))
+
+    bot.sendMessage(chat_id, resp, parse_mode='Markdown')
+
+    response.content_type = 'application/json'
+    return json.dumps({})
+
+run(host='localhost', port=8080)
