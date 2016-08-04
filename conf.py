fc815044066d0d2841cc12b476e742821fbfe1ed
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-08-04 15:33:15 +0300

    added webhook

--- conf.py
+++ conf.py
@@ -6,6 +6,8 @@ TOKEN = '245698813:AAGAwMUKnAk6Zo5pcqSPOzPmSgdaqrmXJs8'
 
 bot = telepot.Bot(TOKEN)
 
+bot.setWebhook('https://rocky-depths-21037.herokuapp.com/{}'.format(TOKEN))
+
 ABUSES = {'adjectives': ('моченый', 'копченый', 'гальванический', 'метафизический', 'веселый',
                          'печальный', 'задорный', 'загадочный', 'пригожий', 'задумчивый',),
           'nouns': ('хуй', 'дрищ', 'лох', 'ботан')}
