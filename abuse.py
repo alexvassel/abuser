b098771280de97eacd16f5bf2c2f44a85b26b4fa
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-08-08 17:57:11 +0300

    use heroku scheduler

--- abuse.py
+++ abuse.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from apscheduler.schedulers.blocking import BlockingScheduler
-
 from conf import bot, PUNEACH_CHAT_ID
 from helpers import get_abuse
 
-scheduler = BlockingScheduler()
-
-
-@scheduler.scheduled_job('cron', hour='3,8,9,15', minute=25)
-def scheduled_job():
-    bot.sendMessage(PUNEACH_CHAT_ID, get_abuse())
-
-scheduler.start()
+bot.sendMessage(PUNEACH_CHAT_ID, get_abuse())
