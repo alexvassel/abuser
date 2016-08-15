4e6fd9ebe401c46bcfd9d2ea7c2af72806e863d3
Aleksandr Vasilev <vasilev@bradburylab.com>
2016-08-15 10:24:49 +0300

    improved the for loop

--- rss_noificator.py
+++ rss_noificator.py
@@ -9,6 +9,9 @@ for key in redis.keys():
     rss_url, chat_id = key.decode().split('|')
     rss_updated = parse(redis.get(key).decode())
     rss = feedparser.parse(rss_url)
+    rss_modified = convert_rss_date_to_datetime(rss.feed.modified)
+    if rss_modified <= rss_updated:
+        continue
     new_entries = get_new_entries(rss, rss_updated)
     send_entries(new_entries, chat_id)
-    redis.set(key, convert_rss_date_to_datetime(rss.feed.modified))
+    redis.set(key, rss_modified)
